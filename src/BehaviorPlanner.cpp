eb6959b7ebbbf7e217a8985c120eb7ca1c87a65d
penny <jombi@web.de>
2017-10-24 22:40:23 +0200

    ADD: BehaviorPlanner class

---
+++ src/BehaviorPlanner.cpp
@@ -0,0 +1,15 @@
+#include "BehaviorPlanner.h"
+
+class BehaviorPlanner
+{
+public:
+  BehaviorPlanner() = default;
+  BehaviorPlanner(BehaviorPlanner &&) = default;
+  BehaviorPlanner(const BehaviorPlanner &) = default;
+  BehaviorPlanner &operator=(BehaviorPlanner &&) = default;
+  BehaviorPlanner &operator=(const BehaviorPlanner &) = default;
+  ~BehaviorPlanner() = default;
+
+private:
+  
+};
---
+++ src/BehaviorPlanner.h
@@ -0,0 +1,13 @@
+#ifndef __BehaviorPlanner_h__
+#define __BehaviorPlanner_h__
+
+class BehaviorPlanner
+{
+public:
+  BehaviorPlanner();
+  ~BehaviorPlanner();
+private:
+  
+};
+
+#endif // __BehaviorPlanner_h__
