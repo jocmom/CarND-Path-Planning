6497798cf99cbdc53675017b050e498d52857e8f
penny <jombi@web.de>
2017-10-24 22:32:12 +0200

    ADD: obstacle struct

---
+++ src/Obstacle.h
@@ -0,0 +1,15 @@
+#ifndef __OBSTACLE_H__
+#define __OBSTACLE_H__
+
+struct Obstacle
+{
+  int id;
+  double x;
+  double y;
+  double vx;
+  double vy;
+  double s;
+  double d;
+};
+
+#endif // __OBSTACLE_H__
