80f34c1b12f3ef34105b154004d83a1b43a99d04
penny <jombi@web.de>
2017-10-27 20:57:47 +0200

    ADD: Constants

---
+++ src/constants.h
@@ -0,0 +1,12 @@
+
+#ifndef __constants_h__
+#define __constants_h__
+
+const double REF_V = 49.5;
+const double MAX_ACC = 10.;
+const double MAX_JERK = 10.;
+const double MAX_V_DELTA = 2.24;
+const double DELTA_T = 0.02;
+const int PATH_POINTS_CNT = 50;
+
+#endif /* __constants_h__ */
