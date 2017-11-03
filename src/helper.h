89a25e8f61a8a518d1c32e9dea263d3a9efa49e1
jocmom <jombi@web.de>
2017-11-03 16:41:58 +0100

    CHG: Moved distance function to helper.h

--- src/helper.h
+++ src/helper.h
@@ -7,4 +7,9 @@ constexpr double pi() { return M_PI; }
 double deg2rad(double x) { return x * pi() / 180; }
 double rad2deg(double x) { return x * 180 / pi(); }
 
+double distance(double x1, double y1, double x2, double y2)
+{
+	return sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+}
+
 #endif // __HELPER_H__
--- src/main.cpp
+++ src/main.cpp
@@ -34,10 +34,6 @@ string hasData(string s) {
   return "";
 }
 
-double distance(double x1, double y1, double x2, double y2)
-{
-	return sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
-}
 int ClosestWaypoint(double x, double y, const vector<double> &maps_x, const vector<double> &maps_y)
 {
 
