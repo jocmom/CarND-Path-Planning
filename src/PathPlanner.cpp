07ed696b3bcac752866dfbdf0814a7bf6dfd670d
jocmom <jombi@web.de>
2017-11-03 15:13:27 +0100

    CHG: Renaming planner

---
+++ src/PathPlanner.cpp
@@ -0,0 +1,15 @@
+#include "PathPlanner.h"
+
+class PathPlanner
+{
+public:
+  PathPlanner() = default;
+  PathPlanner(PathPlanner &&) = default;
+  PathPlanner(const PathPlanner &) = default;
+  PathPlanner &operator=(PathPlanner &&) = default;
+  PathPlanner &operator=(const PathPlanner &) = default;
+  ~PathPlanner() = default;
+
+private:
+  
+};
