cd0bcf080401b878daefaf7c05712c51456771ae
jocmom <jombi@web.de>
2017-11-06 15:06:49 +0100

    CHG: functions have to be inline

--- src/helper.h
+++ src/helper.h
@@ -3,13 +3,15 @@
 
 #include <math.h>
 
-constexpr double pi() { return M_PI; }
-double deg2rad(double x) { return x * pi() / 180; }
-double rad2deg(double x) { return x * 180 / pi(); }
+inline constexpr double pi() { return M_PI; }
+inline double deg2rad(double x) { return x * pi() / 180; }
+inline double rad2deg(double x) { return x * 180 / pi(); }
+inline double speed(double vx, double vy) { return sqrt(vx*vx + vy*vy); }
 
-double distance(double x1, double y1, double x2, double y2)
+inline double distance(double x1, double y1, double x2, double y2)
 {
 	return sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
 }
+
 
 #endif // __HELPER_H__
