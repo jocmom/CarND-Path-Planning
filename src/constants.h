f5b5cf3ae20582003699cf71b44d8404615ae784
jocmom <jombi@web.de>
2017-12-08 12:09:35 +0100

    ADD: MIN_DISTANCE to car ahead

--- src/constants.h
+++ src/constants.h
@@ -9,6 +9,7 @@ const double MAX_V_DELTA = 2.24;
 const double DELTA_T = 0.02;
 const double LANE_WIDTH = 4.0;
 const double CAR_SIZE = 10.0;
+const double MIN_DISTANCE = 25.0;
 constexpr const double TRACK_SIZE = 6945.554;
 const int LANE_CNT = 3;
 const int PATH_POINTS_CNT = 50;
