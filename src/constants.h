7ccb5d9557555d20eaa0995a8913b9d9ac155330
penny <jombi@web.de>
2017-12-17 23:14:57 +0100

    CHG: MAX_DISTANCE

--- src/constants.h
+++ src/constants.h
@@ -5,11 +5,11 @@
 const double REF_V = 49.5;
 const double MAX_ACC = 10.;
 const double MAX_JERK = 10.;
-const double MAX_V_DELTA = 2.24;
+const double MAX_V_DELTA = 0.224;
 const double DELTA_T = 0.02;
 const double LANE_WIDTH = 4.0;
 const double CAR_SIZE = 10.0;
-const double MIN_DISTANCE = 25.0;
+const double MIN_DISTANCE = 30.0;
 constexpr const double TRACK_SIZE = 6945.554;
 const int LANE_CNT = 3;
 const int PATH_POINTS_CNT = 50;
