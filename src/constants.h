d3dceced81c98867f0fe809c31185d23516ad28b
penny <jombi@web.de>
2017-12-26 20:25:06 +0100

    ADD: Optimal distance variable

--- src/constants.h
+++ src/constants.h
@@ -10,6 +10,7 @@ const double DELTA_T = 0.02;
 const double LANE_WIDTH = 4.0;
 const double CAR_SIZE = 10.0;
 const double MIN_DISTANCE = 30.0;
+const double OPTIMAL_DISTANCE = 60.0;
 constexpr const double TRACK_SIZE = 6945.554;
 const int LANE_CNT = 3;
 const int PATH_POINTS_CNT = 50;
